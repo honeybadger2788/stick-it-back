1b6c3712f14c4293b7b2bba8159eb27df2b91f25
honeybadger2788 <noe.carosella@gmail.com>
2021-05-17 21:21:21 -0300

    cargando variables de entorno

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "notes",
+    "database": "playandjoy",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorAliases": false
   },
   "test": {
     "username": "root",
@@ -15,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
